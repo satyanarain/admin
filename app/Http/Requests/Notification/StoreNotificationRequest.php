c4fd44f0a9c207d72847cedadd7c1dcdb312f0ac
satyanarain <satyanarain_chauhan@opiant.in>
2018-07-30 14:39:38 +0530

    m

--- app/Http/Requests/Notification/StoreNotificationRequest.php
+++ app/Http/Requests/Notification/StoreNotificationRequest.php
@@ -26,18 +26,9 @@ class StoreNotificationRequest extends Request
     {
         
             return [
-                   //'name' => 'required',
-                 //  'description' => 'required',
-                   //'price' => 'required',
-                //  'image_name' => 'required',
-                 // 'image_name.required'  => 'image is required',
+                 
               'status'  => 'required',
-                //  'donation_list' => 'required_without_: purchase_list',
-//                   'purchase_list' => 'required_without:donation_list',
-//                  'donation_list' => 'required_without:purchase_list',
-//                'purchase_list' 	=> 'required|exists:users,username,email'
-                   //'purchase_list' => 'required',
-                   //'donation_list' => 'required'
+
                    
              ];
         
